3ea834e0f7940e24664cfb012e03dcff07550c0e
AWSafran <andrewsafran@gmail.com>
2019-03-07 15:45:20 -0500

    created properties in TabLink constructor

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,21 +3,24 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
-    
+    this.element = element;
     // Get the custom data attribute on the Link
     // this.data;
-    
+    this.data = this.element.dataset.tab;
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
+    this.itemElement = document.querySelector(`.tabs-items[data-tab='${this.data}']`);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
+    this.tabItem = new TabItem(this.itemElement);
     // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', () => this.select());
 
-  };
+  }
 
-  select() {
+  select(){
+
+    console.log(`clicked on tab number ${this.data}`)
     // Get all of the elements with the tabs-link class
     // const links;
 
@@ -28,17 +31,20 @@ class TabLink {
     // this.element;
     
     // Call the select method on the item associated with this link
-
   }
+
+  
 }
 
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
     // this.element;
+    this.element = element;
   }
 
   select() {
+    console.log("made it into tab's select function");
     // Select all ".tabs-item" elements from the DOM
     // const items;
 
@@ -59,4 +65,6 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll(".tabs-link").forEach(function(tab){
+  return new TabLink(tab);
+});
